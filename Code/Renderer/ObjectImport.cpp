1cf894ed9e45a55a07b3125dff5bec2f16080a71
josephsil <joseph@catkilo.com>
2025-05-15 23:38:15 -0700

    fixed(?) object import for parents/hiearchies. a lot simpler with submeshes implemented

--- Code/Renderer/ObjectImport.cpp
+++ Code/Renderer/ObjectImport.cpp
@@ -14,22 +14,18 @@ struct AddObjectResult
 };
 void ObjectImport::CreateObjectAssets(Allocator &arena, Scene &scene, AssetManager &assetManager, ImportedObjectData &gltf, DefaultTextures defaults)
 {
-  auto perSubmeshMeshHandles = MemoryArena::AllocSpan<std::span<ID::MeshID>>(arena, gltf.meshes.size());
+    auto perSubmeshMeshHandles = MemoryArena::AllocSpan<std::span<ID::MeshID>>(arena, gltf.meshes.size());
     auto perSubmeshMaterialHandles = MemoryArena::AllocSpan<std::span<ID::MaterialID>>(arena, gltf.meshes.size());
+
+
+
+
     std::span<ID::TextureID> createdTextureIDs = MemoryArena::AllocSpan<ID::TextureID>(arena, gltf.textures.size());
-    std::span<ID::MaterialID> createdMaterialIDs = MemoryArena::AllocSpan<ID::MaterialID>(arena, gltf.materials.size());
-
-    //< Tform
-    int addedCt = 0;
-    std::span<localTransform*> transformPointers = MemoryArena::AllocSpan<localTransform*>(arena, gltf.objects.size());
-    std::span<size_t> parentsPerObjects = MemoryArena::AllocSpanEmplaceInitialize<size_t>(arena, gltf.objects.size(), SIZE_MAX);
-    /// />
-
-
     for (int i = 0; i < gltf.textures.size(); i++)
     {
         createdTextureIDs[i] = assetManager.AddTexture(gltf.textures[i]);
     }
+    std::span<ID::MaterialID> createdMaterialIDs = MemoryArena::AllocSpan<ID::MaterialID>(arena, gltf.materials.size());
     for (int i = 0; i < gltf.materials.size(); i++)
     {
         auto mat = gltf.materials[i];
@@ -49,41 +45,38 @@ void ObjectImport::CreateObjectAssets(Allocator &arena, Scene &scene, AssetManag
         for (int j = 0; j < gltf.meshes[i].submeshes.size(); j++)
         {
             perSubmeshMeshHandles[i][j] = assetManager.AddBackingMesh(gltf.meshes[i].submeshes[j]);
-
-            //The resulting span is the renderer material index, suitable to get passed to create object
-            perSubmeshMaterialHandles[i][j] =   (uint32_t)createdMaterialIDs[gltf.meshes[i].materialIndices[j]];
+            perSubmeshMaterialHandles[i][j] =  createdMaterialIDs[gltf.meshes[i].materialIndices[j]];
         }
     }
 
-    
+    //< Tform
+    std::span<localTransform*> objectTransformReferences = MemoryArena::AllocSpan<localTransform*>(arena, gltf.objects.size());
+    std::span<size_t> parentTransformIndex = MemoryArena::AllocSpanEmplaceInitialize<size_t>(arena, gltf.objects.size(), SIZE_MAX);
+    /// />
     //prepass to set up parents
     for (int i = 0; i < gltf.objects.size(); i++)
     {
         for (int j = 0; j < gltf.objects[i].children.size(); j++)
         {
-            parentsPerObjects[gltf.objects[i].children[j]] = i;
+            parentTransformIndex[gltf.objects[i].children[j]] = i;
         }
     }
-
- 
-    
-    //lol
-
-        for (int i = 0; i < gltf.objects.size(); i++)
+        for (int obj = 0; obj < gltf.objects.size(); obj++)
         {
-            auto object = gltf.objects[i]; //todo js: return list of created parent objects?
+            auto transformData = gltf.objects[obj]; //todo js: return list of created parent objects?
 
-            auto mesh = gltf.meshes[gltf.objects[i].meshidx];
+            auto mesh =gltf.objects[obj].meshidx;
             size_t TODO_J = 0;
-            localTransform* parentTransform = (parentsPerObjects[i] != -1) ? transformPointers[parentsPerObjects[i]] : nullptr;
+            localTransform* parentTransformReference = (parentTransformIndex[obj] != SIZE_MAX) ? objectTransformReferences[parentTransformIndex[obj]] : nullptr;
             size_t objectID = scene.AddObject(
-                perSubmeshMeshHandles[i],
-                perSubmeshMaterialHandles[i],
-                object.translation,
-                object.rotation,
-                object.scale,
-                parentTransform);
-            transformPointers[i] = &scene.transforms.transformNodes[objectID];
+                perSubmeshMeshHandles[mesh],
+                perSubmeshMaterialHandles[mesh],
+                transformData.translation,
+                transformData.rotation,
+                transformData.scale,
+                parentTransformReference);
+            objectTransformReferences[obj] = &scene.transforms.transformNodes[objectID];
+            //TODO setting parent transform(s) is possibly not working right -- could be failing to offset for other objects
         }
 
   
